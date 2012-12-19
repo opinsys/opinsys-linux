4b38d3a1fdf2145d3ede58744fbac4ed87a05a8d
Joseph Salisbury <joseph.salisbury@canonical.com>
2012-12-19 17:52:39 -0500

    UBUNTU: SAUCE: dm-crypt: never use write same
    Not upstream or in linux-next as of Apr 3, 2013.
    
    BugLink: http://bugs.launchpad.net/bugs/1089818
    
    Signed-off-by: Milan Broz <mbroz@redhat.com>
    Signed-off-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1815,6 +1815,12 @@ static int crypt_iterate_devices(struct dm_target *ti,
 	return fn(ti, cc->dev, cc->start, ti->len, data);
 }
 
+static void crypt_io_hints(struct dm_target *ti,
+			    struct queue_limits *limits)
+{
+	limits->max_write_same_sectors = 0;
+}
+
 static struct target_type crypt_target = {
 	.name   = "crypt",
 	.version = {1, 12, 1},
@@ -1829,6 +1835,7 @@ static struct target_type crypt_target = {
 	.message = crypt_message,
 	.merge  = crypt_merge,
 	.iterate_devices = crypt_iterate_devices,
+	.io_hints = crypt_io_hints,
 };
 
 static int __init dm_crypt_init(void)
