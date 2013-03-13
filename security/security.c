551a0c8bf2ef1a66afc0646261f402bcc08e59bd
Paolo Pisati <paolo.pisati@canonical.com>
2013-03-13 12:57:26 +0000

    UBUNTU: SAUCE: compilation fix for missing symbols
    This commit is actually part of
    
    commit 3f7b867ccd7106ee587dcf6882bef9284e1e2d1c
    Author: Andy Whitcroft <apw@canonical.com>
    Date:   Thu Jan 5 14:28:31 2012 +0000
    
        UBUNTU: ubuntu: AUFS -- aufs3-standalone.patch
    
    in P/master, i picked only the relevant EXPORT_SYMBOL().
    
    BugLink: http://bugs.launchpad.net/bugs/1076317
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- security/security.c
+++ security/security.c
@@ -521,6 +521,8 @@ int security_inode_permission(struct inode *inode, int mask)
 	return security_ops->inode_permission(inode, mask);
 }
 
+EXPORT_SYMBOL(security_inode_permission);
+
 int security_inode_setattr(struct dentry *dentry, struct iattr *attr)
 {
 	if (unlikely(IS_PRIVATE(dentry->d_inode)))
