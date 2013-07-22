77397b1af547691e21d41ca6f2a5ce52dfcb8fce
Andy Whitcroft <apw@canonical.com>
2013-07-22 10:29:20 +0100

    Revert "UBUNTU: SAUCE: (no-up) add option to hand off all kernel parameters to init"
    This reverts commit 83bd9f352fa3b3fd7833294ab12ff04e23dd3080.
    
    BugLink: http://bugs.launchpad.net/bugs/1203211
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- include/linux/dynamic_debug.h
+++ include/linux/dynamic_debug.h
@@ -45,7 +45,7 @@ extern __printf(2, 3)
 int __dynamic_pr_debug(struct _ddebug *descriptor, const char *fmt, ...);
 
 extern int ddebug_dyndbg_module_param_cb(char *param, char *val,
-					const char *modname, int all);
+					const char *modname);
 
 struct device;
 
@@ -117,7 +117,7 @@ static inline int ddebug_remove_module(const char *mod)
 }
 
 static inline int ddebug_dyndbg_module_param_cb(char *param, char *val,
-						const char *modname, int all)
+						const char *modname)
 {
 	if (strstr(param, "dyndbg")) {
 		/* avoid pr_warn(), which wants pr_fmt() fully defined */
@@ -125,11 +125,7 @@ static inline int ddebug_dyndbg_module_param_cb(char *param, char *val,
 			"CONFIG_DYNAMIC_DEBUG builds\n");
 		return 0; /* allow and ignore */
 	}
-
-	if (!all)
-		return -EINVAL;
-
-	return 0;
+	return -EINVAL;
 }
 
 #define dynamic_pr_debug(fmt, ...)					\
