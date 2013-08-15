a472ba854f0c531fc4f338515502cd371eb6fef0
John Johansen <john.johansen@canonical.com>
2013-08-15 11:49:36 -0700

    Revert "UBUNTU: SAUCE: apparmor: 3.0 backport fix list_splice_init_rcu 7f708931"
    This reverts commit e7fa0fe7c13889598d00448880264745cc1a1fe5.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- security/apparmor/include/backport.h
+++ security/apparmor/include/backport.h
@@ -59,44 +59,4 @@ static inline int cap_mmap_addr(unsigned long addr)
 #define file_open dentry_open
 #define apparmor_file_open apparmor_dentry_open
 
-/* commit 7f70893173b056df691b2ee7546bb44fd9abae6a */
-#define list_splice_init_rcu(L, H, S) list_splice_init_rcu_fixed((L), (H), (S))
-static inline void list_splice_init_rcu_fixed(struct list_head *list,
-					struct list_head *head,
-					void (*sync)(void))
-{
-	struct list_head *first = list->next;
-	struct list_head *last = list->prev;
-	struct list_head *at = head->next;
-
-	if (list_empty(list))
-		return;
-
-	/* "first" and "last" tracking list, so initialize it. */
-
-	INIT_LIST_HEAD(list);
-
-	/*
-	 * At this point, the list body still points to the source list.
-	 * Wait for any readers to finish using the list before splicing
-	 * the list body into the new list.  Any new readers will see
-	 * an empty list.
-	 */
-
-	sync();
-
-	/*
-	 * Readers are finished with the source list, so perform splice.
-	 * The order is important if the new list is global and accessible
-	 * to concurrent RCU readers.  Note that RCU readers are not
-	 * permitted to traverse the prev pointers without excluding
-	 * this function.
-	 */
-
-	last->next = at;
-	rcu_assign_pointer(list_next_rcu(head), first);
-	first->prev = head;
-	at->prev = last;
-}
-
 #endif /* __AA_BACKPORT_H */
