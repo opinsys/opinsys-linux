d10c2db1a9f7d95a36d0358de0f168db8b71a041
Andy Whitcroft <apw@canonical.com>
2013-10-17 00:39:19 +0100

    UBUNTU: [Config] arm64 -- add generic flavour
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.master/rules.d/arm64.mk
+++ debian.master/rules.d/arm64.mk
@@ -2,12 +2,13 @@ human_arch	= ARMv8
 build_arch	= arm64
 header_arch	= arm64
 defconfig	= defconfig
-flavours	= 
-build_image	= zImage
-kernel_file	= arch/$(build_arch)/boot/zImage
+flavours	= generic
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
 install_file	= vmlinuz
 no_dumpfile = true
 
 loader		= grub
 do_tools	= false
-do_flavour_image_package = false
+#do_flavour_image_package = false
+disable_d_i	= true
