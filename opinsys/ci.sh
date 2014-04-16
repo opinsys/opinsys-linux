d8e158cdbeee7e438b7dab2c77d6f96edd9c9c5c
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2014-04-16 09:37:06 +0300

    opinsys: compile with upstream's own deb-pkg target

--- opinsys/ci.sh
+++ opinsys/ci.sh
@@ -21,16 +21,7 @@ arch=i386
 
 cp "opinsys/config.${arch}" .config
 
-cp -a -r /usr/share/kernel-package /tmp
-cp -a -t /tmp/kernel-package/pkg/image \
-    opinsys/preinst \
-    opinsys/prerm \
-    opinsys/postinst \
-    opinsys/postrm
-cp -a -t /tmp/kernel-package/pkg/headers \
-    opinsys/headers-postinst
-
-CONCURRENCY_LEVEL=4 make-kpkg --initrd --overlay-dir=/tmp/kernel-package --revision="${debian_revision}" kernel_image kernel_headers
+make -j4 deb-pkg INSTALL_MOD_STRIP=1
 
 if [ -n "${APTIREPO_REMOTE:-}" ]; then
     aptirepo-upload -r "${APTIREPO_REMOTE}" -b "kernels" "../linux-image-${upstream_version}_${debian_revision}_${arch}.deb"
