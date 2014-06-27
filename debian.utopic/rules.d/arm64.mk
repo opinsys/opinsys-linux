908d9036c246386bf5f5295fcdb3232a02055184
Tim Gardner <tim.gardner@canonical.com>
2014-06-27 12:42:29 -0600

    UBUNTU: [Config] Created LTS backport branch
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian.utopic/rules.d/arm64.mk
@@ -0,0 +1,25 @@
+human_arch	= ARMv8
+build_arch	= arm64
+header_arch	= arm64
+defconfig	= defconfig
+flavours	= generic
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
+install_file	= vmlinuz
+no_dumpfile = true
+
+loader		= grub
+vdso		= vdso_install
+
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+
+dtb_files_generic += apm-mustang.dtb
+dtb_files_generic += foundation-v8.dtb
+dtb_files_generic += rtsm_ve-aemv8a.dtb
+skipabi		= true
+skipmodule	= true
+do_libc_dev_package=false
+do_source_package=false
+do_doc_package  = false
