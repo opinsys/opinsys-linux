acdd91586e9fcc5bc4f63f8dfb00a96c391fa0b2
Vincent Wan <vincent.wan@amd.com>
2014-11-05 14:09:00 +0800

    mmc: sdhci: Add a quirk for AMD SDHC transfer mode register need to be cleared for cmd without data
    BugLink: https://bugs.launchpad.net/bugs/1472843
    
    SDHC controller in AMD chipsets require SDHC transfer mode
    register to be cleared for commands without data. The issue was
    uncovered during testing eMMC cards on KB/ML based platforms
    
    Signed-off-by: Vincent Wan <vincent.wan@amd.com>
    Signed-off-by: Wan Zongshun <mcuos.com@gmail.com>
    Signed-off-by: Arindam Nath <arindam.nath@amd.com>
    Tested-by: Vikram B <vikram.b@amd.com>
    Tested-by: Raghavendra Swamy <raghavendra.swamy@amd.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (back ported from commit 9b8ffea6efb0d0edcac265a1ca422188fc1b6dfb)
    Signed-off-by: Adam Lee <adam.lee@canonical.com>
    
    Conflicts:
    	include/linux/mmc/sdhci.h
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -98,6 +98,8 @@ struct sdhci_host {
 #define SDHCI_QUIRK2_BROKEN_HS200			(1<<6)
 /* Controller does not support DDR50 */
 #define SDHCI_QUIRK2_BROKEN_DDR50			(1<<7)
+/* need clear transfer mode register before send cmd */
+#define SDHCI_QUIRK2_CLEAR_TRANSFERMODE_REG_BEFORE_CMD	(1<<10)
 
 	int irq;		/* Device IRQ */
 	void __iomem *ioaddr;	/* Mapped address */
