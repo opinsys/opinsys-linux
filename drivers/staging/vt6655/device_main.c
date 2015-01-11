0424df2b9a4f3bf84c4bc682ed2af9b499144425
Malcolm Priestley <tvboxspy@gmail.com>
2015-01-11 10:26:12 +0000

    staging: vt6655: [BUG] Protect MACvSelectPage1 with lock.
    BugLink: http://bugs.launchpad.net/bugs/1471252
    
    commit 95775d12219285d6e0116acb6267864582cd01ef upstream.
    
    The device must not flip to page 1 while in interrupt lock causing
    loss of connection or dead lock.
    
    Protect from changes to page by adding lock where user can
    change the page in CARDvSetRSPINF, vnt_configure and set_channel
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- drivers/staging/vt6655/device_main.c
+++ drivers/staging/vt6655/device_main.c
@@ -1571,6 +1571,10 @@ static void vnt_configure(struct ieee80211_hw *hw,
 
 	if (changed_flags & FIF_ALLMULTI) {
 		if (*total_flags & FIF_ALLMULTI) {
+			unsigned long flags;
+
+			spin_lock_irqsave(&priv->lock, flags);
+
 			if (priv->mc_list_count > 2) {
 				MACvSelectPage1(priv->PortOffset);
 
@@ -1591,6 +1595,8 @@ static void vnt_configure(struct ieee80211_hw *hw,
 
 				MACvSelectPage0(priv->PortOffset);
 			}
+
+			spin_unlock_irqrestore(&priv->lock, flags);
 
 			rx_mode |= RCR_MULTICAST | RCR_BROADCAST;
 		} else {
