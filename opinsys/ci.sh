a21597591533506544a223425e6230e953c0371c
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2015-02-17 14:38:19 +0200

    opinsys/ci.sh: remove git dependencies

--- opinsys/ci.sh
+++ opinsys/ci.sh
@@ -3,7 +3,7 @@
 set -eux
 
 sudo apt-get update
-sudo apt-get install -y --force-yes kernel-wedge kernel-package bison flex dialog libncurses5-dev bc aptirepo-upload git
+sudo apt-get install -y --force-yes kernel-wedge kernel-package bison flex dialog libncurses5-dev bc
 
 version=$(sed -r -n 's/^VERSION = //p' Makefile)
 patchlevel=$(sed -r -n 's/^PATCHLEVEL = //p' Makefile)
@@ -12,10 +12,7 @@ extraversion=$(sed -r -n 's/^EXTRAVERSION = //p' Makefile)
 
 upstream_version="${version}.${patchlevel}.${sublevel}${extraversion}"
 
-git_commit="~$(git rev-parse HEAD)" || {
-    git_commit=
-}
-debian_revision="${upstream_version}-${BUILD_NUMBER:-0}${git_commit}"
+debian_revision="${upstream_version}-0+opinsys1"
 
 arch=i386
 
@@ -31,8 +28,3 @@ cp -a -t /tmp/kernel-package/pkg/headers \
     opinsys/headers-postinst
 
 CONCURRENCY_LEVEL=4 make-kpkg --initrd --overlay-dir=/tmp/kernel-package --revision="${debian_revision}" kernel_image kernel_headers
-
-if [ -n "${APTIREPO_REMOTE:-}" ]; then
-    aptirepo-upload -r "${APTIREPO_REMOTE}" -b "kernels" "../linux-image-${upstream_version}_${debian_revision}_${arch}.deb"
-    aptirepo-upload -r "${APTIREPO_REMOTE}" -b "kernels" "../linux-headers-${upstream_version}_${debian_revision}_${arch}.deb"
-fi
