bittencourtjulival/eclipse_kernel_xiaomi_stone

drivers: net: xgene: constify of_device_id array
of_device_id is always used as const.
(See driver.of_match_table and open firmware functions)

Signed-off-by: Fabian Frederick <fabf@skynet.be>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/apm/xgene/xgene_enet_main.c
@@ -1033,7 +1033,7 @@ MODULE_DEVICE_TABLE(acpi, xgene_enet_acpi_match);
 #endif
 
 #ifdef CONFIG_OF
-static struct of_device_id xgene_enet_of_match[] = {
+static const struct of_device_id xgene_enet_of_match[] = {
 	{.compatible = "apm,xgene-enet",},
 	{.compatible = "apm,xgene1-sgenet",},
 	{.compatible = "apm,xgene1-xgenet",},

opinsys/opinsys-linux

BugLink: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1458042
(backported from commit a6b0dc2af494456b197048b75321a871e13dc398)
Signed-off-by: Phong Vo <pvo@apm.com>
Signed-off-by: Craig Magina <craig.magina@canonical.com>
Conflicts:
	drivers/net/ethernet/apm/xgene/xgene_enet_main.c
Acked-by: Tim Gardner <tim.gardner@canonical.com>
Acked-by: Brad Figg <brad.figg@canonical.com>
Signed-off-by: Brad Figg <brad.figg@canonical.com>

drivers/net/ethernet/apm/xgene/xgene_enet_main.c
@@ -980,7 +980,7 @@ static int xgene_enet_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id xgene_enet_match[] = {
+static const struct of_device_id xgene_enet_match[] = {
 	{.compatible = "apm,xgene-enet",},
 	{.compatible = "apm,xgene1-sgenet",},
 	{.compatible = "apm,xgene1-xgenet",},