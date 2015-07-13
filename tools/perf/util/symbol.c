50e46f1161a6409120d72d25a2146855ac8b8315
Arnaldo Carvalho de Melo <acme@redhat.com>
2015-07-13 08:21:57 -0300

    perf symbols: Store if there is a filter in place
    BugLink: http://bugs.launchpad.net/bugs/1485110
    
    commit 0bc2f2f7d080561cc484d2d0a162a9396bed3383 upstream.
    
    When setting yup the symbols library we setup several filter lists,
    for dsos, comms, symbols, etc, and there is code that, if there are
    filters, do certain operations, like recalculate the number of non
    filtered histogram entries in the top/report TUI.
    
    But they were considering just the "Zoom" filters, when they need to
    take into account as well the above mentioned filters (perf top --comms,
    --dsos, etc).
    
    So store in symbol_conf.has_filter true if any of those filters is in
    place.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-f5edfmhq69vfvs1kmikq1wep@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    [ kamal: 3.19-stable prereq for
      9c0fa8d perf hists browser: Take the --comm, --dsos, etc filters into account ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -1856,6 +1856,8 @@ int setup_list(struct strlist **list, const char *list_str,
 		pr_err("problems parsing %s list\n", list_name);
 		return -1;
 	}
+
+	symbol_conf.has_filter = true;
 	return 0;
 }
 
