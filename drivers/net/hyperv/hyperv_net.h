861d382be9d4e45f7b665db964efe6c61cb2db99
Luis Henriques <luis.henriques@canonical.com>
2015-09-08 14:05:23 +0100

    Revert "hv_netvsc: Implement partial copy into send buffer"
    BugLink: https://bugs.launchpad.net/bugs/1492146
    
    This reverts commit 1a32316060ef59fa89844137e8782198dbb630a0.
    
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- drivers/net/hyperv/hyperv_net.h
+++ drivers/net/hyperv/hyperv_net.h
@@ -133,8 +133,6 @@ struct hv_netvsc_packet {
 	struct hv_device *device;
 	bool is_data_pkt;
 	bool xmit_more; /* from skb */
-	bool cp_partial; /* partial copy into send buffer */
-
 	u16 vlan_tci;
 
 	u16 q_idx;
@@ -148,9 +146,6 @@ struct hv_netvsc_packet {
 
 	/* This points to the memory after page_buf */
 	struct rndis_message *rndis_msg;
-
-	u32 rmsg_size; /* RNDIS header and PPI size */
-	u32 rmsg_pgcnt; /* page count of RNDIS header and PPI */
 
 	u32 total_data_buflen;
 	/* Points to the send/receive buffer where the ethernet frame is */
