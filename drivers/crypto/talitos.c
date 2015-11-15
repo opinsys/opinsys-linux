a158bc3566dfe2e444ba276e3525b74bb5818a77
David Gstir <david@sigma-star.at>
2015-11-15 17:14:42 +0100

    crypto: talitos - Fix timing leak in ESP ICV verification
    BugLink: http://bugs.launchpad.net/bugs/1528359
    
    commit 79960943fdc114fd4583c9ab164b5c89da7aa601 upstream.
    
    Using non-constant time memcmp() makes the verification of the authentication
    tag in the decrypt path vulnerable to timing attacks. Fix this by using
    crypto_memneq() instead.
    
    Signed-off-by: David Gstir <david@sigma-star.at>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    [ kamal: backport to 3.19-stable: context ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/crypto/talitos.c
+++ drivers/crypto/talitos.c
@@ -870,7 +870,7 @@ static void ipsec_esp_decrypt_swauth_done(struct device *dev,
 			icvdata = &edesc->link_tbl[0];
 
 		sg = sg_last(req->dst, edesc->dst_nents ? : 1);
-		err = memcmp(icvdata, (char *)sg_virt(sg) + sg->length -
+		err = crypto_memneq(icvdata, (char *)sg_virt(sg) + sg->length -
 			     ctx->authsize, ctx->authsize) ? -EBADMSG : 0;
 	}
 
